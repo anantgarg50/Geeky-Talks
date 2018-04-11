b077e5962a28141df52a3b84d3775eb28ee0b61b
Anant Garg <anantgarg50@gmail.com>
2018-04-11 16:53:31 +0530

    defined User-Model for database and deleted some extra files

--- server/app.js
+++ server/app.js
@@ -1,49 +1,41 @@
+// Importing Express
+var express = require("express");
 
-// Importing Express 
-var express = require('express');
+// Node Modules
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan"); //Logger
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
 
-// Express Modules
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-// DB Connection
-var db = require('./database/connection');
-
-/*
-// Mongoose Models
-var userSchema = require('./database/models/userModel');
-var postSchema = require('./database/models/postModel');
-var answerSchema = require('./database/models/answerModel');
-*/
-
-// Routes
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-// Express app 
+// Express app
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// DB Connection
+var db = require("./database/connection");
+
+// Mongoose Models
+var User = require("./database/models/userModel");
+//var postSchema = require('./database/models/postModel');
+
+app.get("/", (req, res) => {
+  res.send({
+    message: "Hello World!"
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -52,18 +44,10 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send("error");
 });
-
-
-/*
-let userInstance = new userSchema({name: 'Anant Garg'});
-
-userInstance.save( (err) => { if(err) console.error(err); });
-*/
-
 
 module.exports = app;
--- server/database/connection.js
+++ server/database/connection.js
@@ -1,17 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const uri = "mongodb://mrityunjay:mrityunjay@cluster0-shard-00-00-enrzj.mongodb.net:27017," + 
-		"cluster0-shard-00-01-enrzj.mongodb.net:27017," + 
-		"cluster0-shard-00-02-enrzj.mongodb.net:27017" + 
-		"/test?ssl=true&replicaSet=cluster0-shard-0&authSource=admin";
+const uri =
+  "mongodb://mrityunjay:mrityunjay@cluster0-shard-00-00-enrzj.mongodb.net:27017," +
+  "cluster0-shard-00-01-enrzj.mongodb.net:27017," +
+  "cluster0-shard-00-02-enrzj.mongodb.net:27017" +
+  "/test?ssl=true&replicaSet=cluster0-shard-0&authSource=admin";
 
-mongoose.connect(uri);
+mongoose.connect(uri, function() {
+  console.log("Database Connection Successful!");
+});
 
 mongoose.Promise = global.Promise;
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, "DataBase Connection Error: "));
+db.on("error", console.error.bind(console, "DataBase Connection Error!"));
 
 module.exports = mongoose;
-
